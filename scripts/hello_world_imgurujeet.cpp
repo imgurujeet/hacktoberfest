7da46f2d37c8a8a432e1379fd03ebf4a0ec448c9
imgurujeet <146475585+imgurujeet@users.noreply.github.com>
2024-10-08 00:09:18 +0530

    Added hello world script and updated CONTRIBUTORS.md

---
+++ scripts/hello_world_imgurujeet.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // LANGUAGE: C++
+    // ENV: Vs code
+    // AUTHOR: Gurujeet Kumar
+    // GITHUB: https://github.com/imgurujeet
+
+    cout << "Hello World !!" << endl;
+
+    return 0;
+}
